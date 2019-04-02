436accd5d36a345ecc3ae244461f534dcecb0e5f
bbennett7 <bryn.bennett@gmail.com>
2019-04-02 15:28:31 -0700

    built all routes

--- app.rb
+++ app.rb
@@ -2,22 +2,30 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/reversename/:name' do
-
+    @name = params[:name]
+    "#{@name.reverse}"
   end
 
   get '/square/:number' do
-
+    @number = params[:number].to_i
+    "#{@number * @number}"
   end
 
-  get '/say/:number:phrase' do
-
+  get '/say/:number/:phrase' do
+    erb :say
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @word1 = params[:word1]
+    @word2 = params[:word2]
+    @word3 = params[:word3]
+    @word4 = params[:word4]
+    @word5 = params[:word5]
 
+    "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   end
 
   get '/:operation/:number1/:number2' do
-
-  end 
+    erb :operation
+  end
 end
